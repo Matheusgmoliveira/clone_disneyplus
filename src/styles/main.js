2bdd63b094679ae0e7fcd0c8750a515e02f7bd14
Matheus Gonçalves <matheusgoncalves1970@gmail.com>
2024-03-14 06:08:35 -0300

    ajuste no mobile

--- src/styles/main.js
+++ src/styles/main.js
@@ -1,7 +1,21 @@
 let buttons; // Declare a variável globalmente
-
+let questions
+let heroSection
 document.addEventListener('DOMContentLoaded', function(){
     buttons = document.querySelectorAll('[data-tab-button]');
+    questions = document.querySelectorAll('[data-faq-question]')
+    heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY
+
+        if(posicaoAtual < alturaHero){
+            ocultaElementosDoHeader();
+        }else{
+            exibeElementosDoHeader();
+        }
+    })
     
     for(let i = 0; i < buttons.length; i++){
         buttons[i].addEventListener('click', function(botao){
@@ -13,7 +27,29 @@ document.addEventListener('DOMContentLoaded', function(){
             botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
+
+    for(let i =0; i < questions.length;i++){
+        questions[i].addEventListener('click',abreOuFechaResposta);
+    }
 });
+
+
+function ocultaElementosDoHeader(){
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden')
+}
+
+function exibeElementosDoHeader(){
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden')
+}
+
+function abreOuFechaResposta(elemento){
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe)
+}
 
 function removeBotaoAtivo(){
     for(let i = 0; i < buttons.length; i++){
